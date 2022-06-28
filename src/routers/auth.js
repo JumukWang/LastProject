3839c54291636e633cde1d97e9247c3d9fc3ecbe
JumukWang <dhgus125@gmail.com>
2022-06-29 05:17:35 +0900

    [feat] 유효성검사 불러오기 완료

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,13 +1,20 @@
-require("dotenv").config();
-const { User } = require("../models")
-const jwt = require("jsonwebtoken");
-const passport = require("passport");
-const SALT_NUM = process.env.SALT_NUM;
-const SECRET_KEY = process.env.SECRET_KEY;
-const REFRESH_SECRET_KEY = process.env.REFRESH_SECRET_KEY;
-
 require("dotenv").config()
+require("dotenv").config()
+const { User } = require("../models")
+const jwt = require("jsonwebtoken")
+const passport = require("passport")
+const SALT_NUM = process.env.SALT_NUM
+const SECRET_KEY = process.env.SECRET_KEY
+const REFRESH_SECRET_KEY = process.env.REFRESH_SECRET_KEY
+
 const router = require("express").Router()
+
+const {
+  validateEmail,
+  validateNick,
+  validatePwd,
+} = require("../middlewares/validation")
+
 
 // 회원가입
 router.post("/user", async (req, res, next) => {
@@ -28,7 +35,8 @@ router.post("/user", async (req, res, next) => {
 
 // 로그인
 router.post("/user/auth", async (req, res, next) => {
-  try { // 여기도 중복검사 해야함
+  try {
+    // 여기도 중복검사, 해쉬화된 비밀번호 검증
     const { email, password } = req.body
     const user = await User.findOne({ where: { email } })
 
@@ -39,13 +47,12 @@ router.post("/user/auth", async (req, res, next) => {
       expiresIn: "10d",
     })
 
-    await User.update({ where : { email }, refreshToken })
+    await User.update({ where: { email }, refreshToken })
 
-    res.status(200).send({ 
-        msg : "로그인 되었습니다",
-        token : accessToken
+    res.status(200).send({
+      msg: "로그인 되었습니다",
+      token: accessToken,
     })
-
   } catch (error) {
     return res.status(400).send({
       msg: "다시 회원가입을 신청해 주세요",
@@ -60,9 +67,7 @@ router.get("/user/exemail", async (req, res, next) => {})
 // 닉네임 중복검사
 router.get("/user/exnickname", async (req, res, next) => {})
 
-
-
-router.get("/kakao", passport.authenticate("kakao"));
+router.get("/kakao", passport.authenticate("kakao"))
 
 router.get(
   "/kakao/callback",
@@ -70,32 +75,32 @@ router.get(
     failureRedirect: "/",
   }),
   (req, res) => {
-    res.redirect("/");
+    res.redirect("/")
   }
-);
+)
 
 // 카카오 로그아웃
 // auth//kakao/logout
-router.get('/kakao/logout', async (req,res)=>{
+router.get("/kakao/logout", async (req, res) => {
   // https://kapi.kakao/com/v1/user/logout
   try {
-    const ACCESS_TOKEN = res.locals.user.accessToken;
+    const ACCESS_TOKEN = res.locals.user.accessToken
     let logout = await axios({
-      method:'post',
-      url:'https://kapi.kakao.com/v1/user/unlink',
-      headers:{
-        'Authorization': `Bearer ${ACCESS_TOKEN}`
-      }
-    });
+      method: "post",
+      url: "https://kapi.kakao.com/v1/user/unlink",
+      headers: {
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+      },
+    })
   } catch (error) {
-    console.error(error);
-    res.json(error);
+    console.error(error)
+    res.json(error)
   }
   // 세션 정리
-  req.logout();
-  req.session.destroy();
-  
-  res.redirect('/');
+  req.logout()
+  req.session.destroy()
+
+  res.redirect("/")
 })
 
 module.exports = router
