90aeac9605523438ba04a7a0e45184d84ec9af1c
Ohhyeon Jung <96240712+JumukWang@users.noreply.github.com>
2022-06-30 21:52:55 +0900

    Delete routes directory

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config()
-const router = require("express").Router();
-
-
-router.get('/get' ,(req, res, next) => {
-    res.send({ message: "ㅅㅂ 제발...."});
-})
-
-module.exports = router
