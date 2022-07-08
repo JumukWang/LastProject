d6639965a4cc61781a5f2fdf1b59980a1c948b84
JumukWang <dhgus125@gmail.com>
2022-07-08 18:30:48 +0900

    [fix]코멘트 추가

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
-const {Room, User} = require("../models")
-const router = require("express").Router();
+const { Room, User } = require("../models")
+const router = require("express").Router()
 const authMiddleware = require("../middlewares/authmiddleware")
 
 // 메인 페이지
@@ -9,7 +9,7 @@ router.get("/", async (req, res, next) => {
     const roomList = await Room.find({})
     return res.status(201).send({
       result: true,
-      roomList
+      roomList,
     })
   } catch (error) {
     return res.status(400).send({
@@ -22,28 +22,30 @@ router.get("/", async (req, res, next) => {
 // 유저에 배열 만들어서 저장해야함
 // res.locals.user 못쓰니까 불러와서 써야함
 router.post("/like/:roomId", authMiddleware, async (req, res, next) => {
-    let flag = false;
-    const roomId = Number(req.params.roomId)
-    if(roomId){
-      let flag = true
-      await Room.updateOne({roomId}, {$set: { isLiked: flag}})
-    }
-    return res.status(201).send({
-      result: true,
-      msg: "스터디룸 좋아요를 눌렀습니다."
-    })
+  let flag = false
+  const roomId = Number(req.params.roomId)
+  if (roomId) {
+    let flag = true
+    await Room.updateOne({ roomId }, { $set: { isLiked: flag } })
+  }
+  // 여기서 배열에 저장하면 될 듯 합니다
+  return res.status(201).send({
+    result: true,
+    msg: "스터디룸 좋아요를 눌렀습니다.",
+  })
 })
 
 router.post("/dislike/:roomId", authMiddleware, async (req, res, next) => {
-  let flag = true;
+  let flag = true
   const roomId = Number(req.params.roomId)
-  if(roomId){
+  if (roomId) {
     let flag = false
-    await Room.updateOne({roomId}, {$set: { isLiked: flag}})
+    await Room.updateOne({ roomId }, { $set: { isLiked: flag } })
   }
+  // 여기서 배열에 저장하면 될 듯 합니다
   return res.status(201).send({
     result: true,
-    msg: "스터디룸 좋아요를 취소했습다."
+    msg: "스터디룸 좋아요를 취소했습다.",
   })
 })
 
