e707268a57045aeee6422279e208e2f52654d8dc
JumukWang <dhgus125@gmail.com>
2022-07-30 03:05:34 +0900

    [fix] test

--- switchServer.sh
+++ switchServer.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
  
 # Blue 를 기준으로 현재 떠있는 컨테이너를 체크한다.
-EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yaml ps | grep Up)
+EXIST_BLUE=$(docker-compose -p last-project:latest-blue -f docker-compose.blue.yaml ps | grep Up)
  
 # 컨테이너 스위칭
 if [ -z "$EXIST_BLUE" ]; then
     echo "blue up"
-    docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yaml up -d
+    docker-compose -p last-project-blue -f docker-compose.blue.yaml up -d
     BEFORE_COMPOSE_COLOR="green"
     AFTER_COMPOSE_COLOR="blue"
 else
     echo "green up"
-    docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yaml up -d
+    docker-compose -p last-project-green -f docker-compose.green.yaml up -d
     BEFORE_COMPOSE_COLOR="blue"
     AFTER_COMPOSE_COLOR="green"
 fi
@@ -19,9 +19,9 @@ fi
 sleep 10
  
 # 새로운 컨테이너가 제대로 떴는지 확인
-EXIST_AFTER=$(docker-compose -p ${DOCKER_APP_NAME}-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yaml ps | grep Up)
+EXIST_AFTER=$(docker-compose -p last-project-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yaml ps | grep Up)
 if [ -n "$EXIST_AFTER" ]; then
     # 이전 컨테이너 종료
-    docker-compose -p ${DOCKER_APP_NAME}-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yaml down
+    docker-compose -p last-project-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yaml down
     echo "$BEFORE_COMPOSE_COLOR down"
 fi
