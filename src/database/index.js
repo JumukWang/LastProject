6b695fadb349bfcc45d1ed4dc9aa509a8263b652
YechanChae <chsg2036@gmail.com>
2022-08-01 23:00:13 +0900

    수정

--- src/database/index.js
+++ src/database/index.js
@@ -2,29 +2,6 @@ const config = require('../config');
 
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
-// const connect = async () => {
-//   await mongoose
-//     .connect(
-//       `mongodb+srv://${config.MONGO_USERNAME}:${config.MONGO_PASSWORD}@${config.MONGO_URL}/?retryWrites=true&w=majority`,
-//       {
-//         ignoreUndefined: true,
-//       },
-//     )
-//     .catch((err) => {
-//       console.error(err);
-//     });
-// };
-
-const connect = () => {
-  mongoose.connect(config.MONGO_URL, { ignoreUndefined: true }).catch((err) => {
-    console.error(err);
-  });
-};
-
-// 여기에 스키마 함수 만들기
-
-=======
 const connect = async () => {
   await mongoose
     .connect(
@@ -38,5 +15,12 @@ const connect = async () => {
     });
 };
 
->>>>>>> 7a16f0b2ec985004b01594fe3846a792533a8e1e
+// const connect = () => {
+//   mongoose.connect(config.MONGO_URL, { ignoreUndefined: true }).catch((err) => {
+//     console.error(err);
+//   });
+// };
+
+// 여기에 스키마 함수 만들기
+
 module.exports = connect;
